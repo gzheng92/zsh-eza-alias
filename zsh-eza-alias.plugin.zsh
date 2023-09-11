389301027e5935fb26e5478b9586669a4554d722
Geoff Zheng <42302419+gzheng92@users.noreply.github.com>
2023-09-11 12:46:07 -0400

    Create zsh-eza-alias.plugin.zsh

---
+++ zsh-eza-alias.plugin.zsh
@@ -0,0 +1,13 @@
+if ! (( $+commands[exa] )); then
+  print "zsh-exa-plugin: exa not found on path. Please install exa before using this plugin." >&2
+  return 1
+fi
+
+alias ls='exa -a --icons --git --group-directories-first --time-style=long-iso'
+alias lz='exa --icons --git --group-directories-first --time-style=long-iso'
+alias l='ls -lbF'
+alias ll='ls -la'
+alias llm='ll --sort=modified'
+alias la='ls -lbhHigUmuSa'
+alias lx='ls -lbhHigUmuSa@'
+alias tree='exa --tree'
